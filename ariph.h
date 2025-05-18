5af5bf7ca4ef16f6e9afee102ecc54d94c3abda0
kr-bog <kirill2005b@gmail.com>
2025-05-18 19:05:01 +0300

    Update ariph.h

---
+++ ariph.h
@@ -0,0 +1,11 @@
+#ifndef ARIPH_H
+#define ARIPH_H
+
+#include "matrix.h"
+
+//сложение и умножение
+int matrix_add(matrix *m1, const matrix *m2);
+int matrix_mul(matrix *m1, const matrix *m2);
+
+#endif
+
