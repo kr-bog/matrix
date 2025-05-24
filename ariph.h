34fdb7f2b43f49a255e493e1af7f67fdc38434c8
kr-bog <kirill2005b@gmail.com>
2025-05-24 15:46:07 +0300

    Update ariph.h

--- ariph.h
+++ ariph.h
@@ -1,11 +1,13 @@
 #ifndef ARIPH_H
 #define ARIPH_H
-
 #include "matrix.h"
 
-//сложение и умножение
-int matrix_add(matrix *m1, const matrix *m2);
-int matrix_mul(matrix *m1, const matrix *m2);
+int matrix_add(matrix* m1, const matrix* m2);
+int matrix_sub(matrix* m1, const matrix* m2);
+int matrix_mul(matrix* m1, const matrix* m2);
+int matrix_div(matrix* m1, const matrix* m2); //поэлементное деление
 
-#endif
+int matrix_exp(matrix* m);  //экспонента каждого элемента
+int matrix_gauss(matrix* m); //exp для каждого элемента
 
+#endif //ARIPH_H
