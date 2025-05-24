a5d03fcf251b6c42c939ee6fbe5217ba5229281e
kr-bog <kirill2005b@gmail.com>
2025-05-24 15:47:18 +0300

    Update main.c

--- main.c
+++ main.c
@@ -7,11 +7,10 @@ int main(void) {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    //создаем две матрицы 3 на 3
-    matrix *a = matrix_alloc_id(3, 3);
-    matrix *b = matrix_alloc_id(3, 3);
+    matrix* a = matrix_alloc_id(3, 3);
+    matrix* b = matrix_alloc_id(3, 3);
     if (!a || !b) {
-        printf("ошибка выделения памяти\n");
+        printf("Ошибка выделения памяти\n");
         return 1;
     }
 
@@ -25,7 +24,21 @@ int main(void) {
     printf("\nA + B:\n");
     matrix_print(a);
 
+    matrix_sub(a, b);
+    printf("\n(A + B) - B:\n");
+    matrix_print(a);
+
+    matrix_exp(a);
+    printf("\nexp(A):\n");
+    matrix_print(a);
+
+    matrix_gauss(a);
+    printf("\nGauss(A):\n");
+    matrix_print(a);
+
     matrix_free(a);
     matrix_free(b);
+
     return 0;
 }
+
